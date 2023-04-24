9371c0b79d98c21636189fdbdfdd08e163718a60
yvan-allioux <yvan.allioux.pro@gmail.com>
2023-04-25 00:57:12 +0200

    first commit AES

---
+++ AES.py
@@ -0,0 +1,72 @@
+from Crypto.Cipher import AES
+from Crypto.Random import get_random_bytes
+from Crypto.Util.Padding import pad, unpad
+import base64
+
+def encrypt_aes(plain_text, key):
+    cipher = AES.new(key, AES.MODE_CBC)
+    cipher_text = cipher.encrypt(pad(plain_text.encode(), AES.block_size))
+    return cipher.iv, cipher_text
+
+def decrypt_aes(cipher_text, key, iv):
+    cipher = AES.new(key, AES.MODE_CBC, iv)
+    plain_text = unpad(cipher.decrypt(cipher_text), AES.block_size).decode()
+    return plain_text
+
+def generate_key():
+    key = get_random_bytes(32)
+    key_base64 = base64.b64encode(key).decode()
+    return key_base64
+
+def encrypt_aes_base64(plain_text, key_user_input):
+    key = base64.b64decode(key_user_input.encode()) # Convertir la clé base64 en octets
+    iv, cipher_text = encrypt_aes(plain_text, key)
+    final_cipher_text = base64.b64encode(iv + cipher_text).decode()
+    return final_cipher_text
+
+def decrypt_aes_base64(encrypted_text, key_user_input):
+    key = base64.b64decode(key_user_input.encode()) # Convertir la clé base64 en octets
+    iv = base64.b64decode(encrypted_text.encode())[:AES.block_size] # Extraire l'IV
+    encrypted_text = base64.b64decode(encrypted_text.encode())[AES.block_size:] # Extraire le texte chiffré
+    decrypted_text = decrypt_aes(encrypted_text, key, iv)
+    return decrypted_text
+
+
+#interact with the user
+
+print("MENU AES ENCRYPTION")
+print("1 Encrypt")
+print("2 Decrypt")
+print("3 New Key generation")
+print("4 Exit\n")
+
+mode = input()
+if mode == "1":
+    print("Message :")
+    text_user_input = input()
+    print("Key :")
+    key_user_input = input()
+
+    final_cipher_text = encrypt_aes_base64(text_user_input, key_user_input)
+    print(f"IV + Texte chiffré : \n{final_cipher_text}")
+
+elif mode == "2":
+    print("Message :")
+    text_user_input = input()
+    print("Key :")
+    key_user_input = input()
+    
+    decrypted_text = decrypt_aes_base64(text_user_input, key_user_input)
+    print(f"Texte déchiffré : \n{decrypted_text}")
+    
+elif mode == "3":
+    # Générer une clé AES de 256 bits
+    key_base64 = generate_key()
+    print(f"Clé en base64: {key_base64}")
+
+elif mode == "4":
+    print("Exiting...")
+
+
+else:
+    print("Please enter a valid mode")
---
+++ README.md
@@ -0,0 +1,24 @@
+# AES Encryption Tool
+
+This is a simple, browser-based AES encryption and decryption tool. It allows users to securely encrypt and decrypt messages using the Advanced Encryption Standard (AES) algorithm and a secret key. This tool is designed to provide confidentiality for sensitive data exchanged between parties.
+
+## Features
+
+Encrypt and decrypt messages using AES
+Generate secure AES-256 keys
+Easy-to-use, browser-based interface
+No server-side processing, ensuring data privacy
+
+## Usage
+
+Clone or download the repository to your local machine.
+Open the index.html file in your favorite modern web browser.
+Enter your message in the "Message" textarea.
+Enter your secret key in the "Key" input field. Alternatively, click on the "New Key generation" button to generate a secure AES-256 key.
+Click on the "Encrypt" button to encrypt your message. The encrypted message will be displayed in the output area.
+To decrypt an encrypted message, enter the encrypted message in the "Message" textarea, provide the same secret key, and click on the "Decrypt" button. The decrypted message will be displayed in the output area.
+
+## Security Considerations
+
+This tool uses the AES algorithm, which is widely recognized for its robustness and security. However, the security provided by this tool also depends on how it is used. It is crucial to select a sufficiently long and complex secret key to prevent brute-force attacks. Additionally, make sure to share the secret key only with authorized parties to ensure the confidentiality of communications.
+
